6ade195d32a32dd09a2092c4e3611f433b2f27a2
knaticat <59741693+knaticat@users.noreply.github.com>
2020-01-10 23:01:44 +0530

    Add files via upload

---
+++ nucleo_count.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+def SeqCounter(Seq):
+    a,g,t,c=0,0,0,0
+    
+    for i in range(len(Seq)):
+        if Seq[i]=="A":
+            a+=1
+        elif Seq[i]=="G":
+            g+=1 
+        elif Seq[i]=="T":
+            t+=1
+        elif Seq[i]=="C":
+            c+=1
+        else:
+            print("Sequence Invalid")
+            break
+    return print(str(a)+" "+str(c)+" "+str(g)+" "+str(t))       
+
+
